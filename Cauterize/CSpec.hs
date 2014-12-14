941788ac5b26d1b990f195ce5d7c3cb39bb917a8
John Van Enk <vanenkj@gmail.com>
2014-12-14 03:18:32 -0800

    Flatten out the CTypeDetails a bit more. Should have all the fields we need.

--- Cauterize/CSpec.hs
+++ Cauterize/CSpec.hs
@@ -28,14 +28,20 @@ data CType = CType { ctName :: Text
   deriving (Data, Typeable, Show)
 
 data CTypeDetails = CBuiltIn
-                  | CScalar
-                  | CConst
-                  | CArray
-                  | CVector
-                  | CStruct
-                  | CSet
-                  | CEnum
-                  | CPad
+                  | CConst  { ctdReprName :: Text, ctdConstVal :: Integer }
+                  | CArray  { ctdReprName :: Text, ctdArrayLen :: Integer }
+                  | CVector { ctdReprName :: Text, ctdVectorMaxLen :: Integer, ctdVectorMaxLenReprName :: Text }
+                  | CScalar { ctdReprName :: Text }
+                  | CStruct { ctdFields :: [CNamedRef] }
+                  | CEnum   { ctdFields :: [CNamedRef], ctdEnumTagReprName :: Text }
+                  | CSet    { ctdFields :: [CNamedRef], ctdSetFlagsReprName :: Text }
+                  | CPad    { ctdPadLen :: Integer }
+  deriving (Data, Typeable, Show)
+
+data CNamedRef = CNamedRef { cnrName :: Text
+                           , cnrRefName :: Text
+                           , cnrIndex :: Integer
+                           }
   deriving (Data, Typeable, Show)
 
 mkCSpec :: Sp.Spec -> CSpec
@@ -55,24 +61,12 @@ mkCType t = CType { ctName = pack $ Sp.typeName t
                   }
 
 mkCTypeDetails :: Sp.SpType -> CTypeDetails
-mkCTypeDetails Sp.BuiltIn { Sp.unBuiltIn = Sp.TBuiltIn {} } = CBuiltIn
-mkCTypeDetails Sp.Scalar  { Sp.unScalar  = Sp.TScalar  {} } = CScalar
-mkCTypeDetails Sp.Const   { Sp.unConst   = Sp.TConst   {} } = CConst
-mkCTypeDetails Sp.Array   { Sp.unFixed   = Sp.TArray   {} } = CArray
-mkCTypeDetails Sp.Vector  { Sp.unBounded = Sp.TVector  {} } = CVector
-mkCTypeDetails Sp.Struct  { Sp.unStruct  = Sp.TStruct  {} } = CStruct
-mkCTypeDetails Sp.Set     { Sp.unSet     = Sp.TSet     {} } = CSet
-mkCTypeDetails Sp.Enum    { Sp.unEnum    = Sp.TEnum    {} } = CEnum
-mkCTypeDetails Sp.Pad     { Sp.unPad     = Sp.TPad     {} } = CPad
-
-{-
-mkCType t@(Sp.BuiltIn {}) = CBuiltIn { ctName = pack $ Sp.typeName t }
-mkCType t@(Sp.Scalar {}) = CBuiltIn { ctName = pack $ Sp.typeName t }
-mkCType t@(Sp.Const {}) = CBuiltIn { ctName = pack $ Sp.typeName t }
-mkCType t@(Sp.Array {}) = CBuiltIn { ctName = pack $ Sp.typeName t }
-mkCType t@(Sp.Vector {}) = CBuiltIn { ctName = pack $ Sp.typeName t }
-mkCType t@(Sp.Struct {}) = CBuiltIn { ctName = pack $ Sp.typeName t }
-mkCType t@(Sp.Set {}) = CBuiltIn { ctName = pack $ Sp.typeName t }
-mkCType t@(Sp.Enum {}) = CBuiltIn { ctName = pack $ Sp.typeName t }
-mkCType t@(Sp.Pad {}) = CBuiltIn { ctName = pack $ Sp.typeName t }
--}
+mkCTypeDetails Sp.BuiltIn { Sp.unBuiltIn = Sp.TBuiltIn {} } = CBuiltIn {}
+mkCTypeDetails Sp.Scalar  { Sp.unScalar  = Sp.TScalar  {} } = CScalar  {}
+mkCTypeDetails Sp.Const   { Sp.unConst   = Sp.TConst   {} } = CConst   {}
+mkCTypeDetails Sp.Array   { Sp.unFixed   = Sp.TArray   {} } = CArray   {}
+mkCTypeDetails Sp.Vector  { Sp.unBounded = Sp.TVector  {} } = CVector  {}
+mkCTypeDetails Sp.Struct  { Sp.unStruct  = Sp.TStruct  {} } = CStruct  {}
+mkCTypeDetails Sp.Set     { Sp.unSet     = Sp.TSet     {} } = CSet     {}
+mkCTypeDetails Sp.Enum    { Sp.unEnum    = Sp.TEnum    {} } = CEnum    {}
+mkCTypeDetails Sp.Pad     { Sp.unPad     = Sp.TPad     {} } = CPad     {}
