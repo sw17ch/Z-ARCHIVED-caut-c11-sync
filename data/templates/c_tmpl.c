89075aa53c4750e199670955260c3fd25699794f
John Van Enk <vanenkj@gmail.com>
2015-01-19 21:00:16 -0800

    Actually use the right hash for each type.

--- data/templates/c_tmpl.c
+++ data/templates/c_tmpl.c
@@ -1,11 +1,12 @@
 #include "{{cLibName}}.h"
 
+{{#specInfo}}
 hashtype_t const SCHEMA_HASH_{{cLibName}} = 
   { {{cLibHashStr}} };
 
 {{#cLibTypes}}
 hashtype_t const TYPE_HASH_{{cLibName}}_{{ctName}} =
-  { {{cLibHashStr}} };
+  { {{ctHashStr}} };
 {{/cLibTypes}}
 
 {{> c.pack_tmpl.c}}
@@ -13,3 +14,4 @@ hashtype_t const TYPE_HASH_{{cLibName}}_{{ctName}} =
 {{> c.packed_size_tmpl.c}}
 {{> c.init_tmpl.c}}
 {{> c.order_tmpl.c}}
+{{/specInfo}}
