6909345b1e3a610b3c6a4053304831163d9c6769
John Van Enk <vanenkj@gmail.com>
2015-03-01 13:21:50 -0800

    Remove socket99 references. (Thanks for socke99, Scott)

--- gen-meta/Main.hs
+++ gen-meta/Main.hs
@@ -88,8 +88,4 @@ render spec meta path = do
       renderMakefile spec meta >>= T.writeFile (path `combine` makefileName)
     copyFiles = do
       greatest_dot_h <- getDataFileName "support/greatest.h"
-      socket99_h <- getDataFileName "support/socket99.h"
-      socket99_c <- getDataFileName "support/socket99.c"
       copyFile greatest_dot_h (path `combine` "greatest.h")
-      copyFile socket99_h (path `combine` "socket99.h")
-      copyFile socket99_c (path `combine` "socket99.c")
